11e955ced358efb8273074811918485c1c60680b
ThatGuyGilby <jack.m.gilbride@gmail.com>
2022-02-08 10:43:37 +0000

    Employee Record Added

---
+++ src/main/java/com/teamsix/employees/Employee.java
@@ -0,0 +1,17 @@
+package com.teamsix.employees;
+
+import java.util.Date;
+
+public class Employee
+{
+    private int empID;
+    private String namePrefix;
+    public String firstName;
+    public String middleInitial;
+    public String lastName;
+    public char gender;
+    public String email;
+    public Date dateOfBirth;
+    public Date dateOfJoining;
+    public float salary;
+}
