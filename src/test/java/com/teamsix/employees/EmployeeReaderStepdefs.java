4eeb2a8c49a66627cd382b57e24e00d50a11ee90
ThatGuyGilby <jack.m.gilbride@gmail.com>
2022-02-09 13:53:57 +0000

    Missing File Fix

--- src/test/java/com/teamsix/employees/EmployeeReaderStepdefs.java
+++ src/test/java/com/teamsix/employees/EmployeeReaderStepdefs.java
@@ -7,7 +7,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeReaderStepdefs
@@ -37,5 +39,35 @@ public class EmployeeReaderStepdefs
     public void theResultShouldBeAListOfEmployees()
     {
         Assertions.assertNotNull(employees);
+    }
+
+    @Then("the result should be a list of unique employees")
+    public void theResultShouldBeAListOfUniqueEmployees()
+    {
+        List<Integer> usedIDs = new ArrayList<>();
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < employees.size(); i++)
+        {
+            if (!usedIDs.contains(employees.get(i).empID))
+            {
+                usedIDs.add(employees.get(i).empID);
+            }
+            else
+            {
+                duplicateFound = true;
+            }
+        }
+
+        Assert.assertFalse(duplicateFound);
+    }
+
+    @Then("names and surnames should have only letters")
+    public void namesAndSurnamesShouldHaveOnlyLetters() {
+        for(Employee employee: employees){
+            if (!(employee.firstName+employee.lastName).matches("^[a-zA-Z]*$")){
+                Assertions.fail();
+            }
+        }
     }
 }
